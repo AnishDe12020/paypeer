c0d570cabd60c55d2f1aade62595861a9347810e
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-12-11 18:52:42 +0530

    feat: add tx success msg 😎

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,22 +8,44 @@ import {
   Heading,
   Input,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import { createQR, encodeURL, TransferRequestURLFields } from "@solana/pay";
+import {
+  createQR,
+  encodeURL,
+  findReference,
+  FindReferenceError,
+  TransferRequestURLFields,
+  validateTransfer,
+  ValidateTransferError,
+} from "@solana/pay";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair, PublicKey } from "@solana/web3.js";
 import BigNumber from "bignumber.js";
 import { useEffect, useMemo, useRef, useState } from "react";
 
+const USDC_ADDRESS = new PublicKey(
+  "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"
+);
+
+enum TxStatus {
+  PENDING = "pending",
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
 export default function Home() {
+  const { connection } = useConnection();
   const { publicKey } = useWallet();
   const [amount, setAmount] = useState<string | undefined>();
+  const [reference, setReference] = useState<string | undefined>();
   const [size, setSize] = useState(() =>
     typeof window === "undefined"
       ? 400
       : Math.min(window.screen.availWidth - 48, 400)
   );
+  const [txStatus, setTxStatus] = useState<TxStatus>();
 
   useEffect(() => {
     if (typeof window === "undefined") return;
@@ -36,14 +58,40 @@ export default function Home() {
 
   const qrRef = useRef<HTMLDivElement>(null);
 
-  const reference = useMemo(() => Keypair.generate().publicKey, []);
+  const toast = useToast();
 
   const gen = async () => {
+    if (!publicKey) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet to continue",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    if (!amount) {
+      toast({
+        title: "Amount not specified",
+        description: "Please enter the amount to continue",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    const ref = Keypair.generate().publicKey.toString();
+
+    setReference(ref);
+
     const urlParams: TransferRequestURLFields = {
-      recipient: publicKey as PublicKey,
-      splToken: new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"),
-      amount: new BigNumber(amount as string),
-      reference,
+      recipient: publicKey,
+      splToken: USDC_ADDRESS,
+      amount: new BigNumber(amount),
+      reference: new PublicKey(ref),
       label: "Merchant Inc",
       message: "Thanks for your order!",
     };
@@ -57,7 +105,103 @@ export default function Home() {
       qrRef.current.innerHTML = "";
       qr.append(qrRef.current);
     }
+
+    setTxStatus(TxStatus.PENDING);
   };
+
+  useEffect(() => {
+    if (!publicKey) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet to continue",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    if (!amount) {
+      toast({
+        title: "Amount not specified",
+        description: "Please enter the amount to continue",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    if (txStatus !== TxStatus.PENDING) return;
+
+    console.log("polling...", reference);
+
+    const interval = setInterval(async () => {
+      try {
+        const signatureInfo = await findReference(
+          connection,
+          new PublicKey(reference as string),
+          {
+            finality: "confirmed",
+          }
+        );
+
+        console.log("signatureInfo", signatureInfo);
+
+        await validateTransfer(
+          connection,
+          signatureInfo.signature,
+          {
+            recipient: publicKey,
+            amount: new BigNumber(amount),
+            splToken: USDC_ADDRESS,
+            reference: new PublicKey(reference as string),
+          },
+          { commitment: "confirmed" }
+        );
+
+        console.log("success");
+
+        setTxStatus(TxStatus.SUCCESS);
+
+        toast({
+          title: "Amount paid!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } catch (e) {
+        if (e instanceof FindReferenceError) {
+          // No transaction found yet, ignore this error
+          console.log("nah");
+          return;
+        }
+        if (e instanceof ValidateTransferError) {
+          setTxStatus(TxStatus.ERROR);
+          console.error("Transaction is invalid", e);
+          toast({
+            title: "Transaction is invalid",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          });
+          return;
+        }
+        setTxStatus(TxStatus.ERROR);
+        toast({
+          title: "Unknown error",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        console.error("Unknown error", e);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [txStatus]);
 
   return (
     <Container>
