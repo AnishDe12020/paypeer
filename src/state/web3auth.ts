c52b6a7238244ae1892bae159365d959622856fd
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-01-01 18:52:39 +0530

    chore: remove web3auth

--- src/state/web3auth.ts
+++
@@ -1,6 +0,0 @@
-import { SafeEventEmitterProvider } from "@web3auth/base";
-import { Web3Auth } from "@web3auth/modal";
-import { atom } from "jotai";
-
-export const web3authAtom = atom<Web3Auth | null>(null);
-export const providerAtom = atom<SafeEventEmitterProvider | null>(null);
