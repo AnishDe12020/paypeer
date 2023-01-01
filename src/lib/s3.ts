09d64d069894546987e9809765b59bfc4a578837
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-01-01 18:47:06 +0530

    chore: rename aws env vars

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,8 +3,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   region: process.env.NEXT_PUBLIC_AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY!,
   },
 });
 
